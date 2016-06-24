dee46af02883249b5d8d6b28c98cc792cd9b9e86
Jason Yeomans <jyeo098@gmail.com>
2016-06-24 14:43:27 -0400

    env prod update

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -6,12 +6,12 @@ module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
             process.env.IP ||
-            undefined,
+            "0.0.0.0",
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
-            8080,
+            "9024",
 
   // MongoDB connection options
   mongo: {
